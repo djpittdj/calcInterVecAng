5a241948948dfe87a802bfa4f34e38c598faef8c
Jian Dai <djpittdj@gmail.com>
2015-01-05 12:17:21 -0500

    Update calcInterVecAng.py

--- calcInterVecAng.py
+++ calcInterVecAng.py
@@ -1,12 +1,12 @@
+#!/usr/bin/python
 """
 Author: Jian Dai
 """
-#!/usr/bin/python
 from math import sin, cos, pi, acos
 import numpy as np
 
 class CalcInterVecAng:
-	def __init__(self, infilename, outfilename):
+	def __init__(self, infilename, outfilename, isStruct=False):
 		self.outfilename = outfilename
 		phipsi_lst = []
 		for line in open(infilename, 'r'):
@@ -15,6 +15,7 @@ class CalcInterVecAng:
 			psi = float(line.split()[3])
 			phipsi_lst.append([phi, psi, resid1])
 		self.phipsi_lst = phipsi_lst
+		self.isStruct = isStruct
 
 	def R1(self, angle):
 		angle = angle * pi/180.0
@@ -33,22 +34,32 @@ class CalcInterVecAng:
 	def calcOneInterVecAng(self, i, j):
 		resid_i = self.phipsi_lst[i][2]
 		resid_j = self.phipsi_lst[j][2]
-		# transformation matrix from DFF to PAF
-		tt = np.array([[0.719, 0.0, -0.695], \
-			           [-0.695, 0.0, -0.719], \
-			           [0.0, 1.0, 0.0]])
-		extAlpha = 65.0
-		extBeta = 59.0
-		extGamma = 70.0
+		# transformation matrix from PAF to DFF, page 53 of Jun Hu's thesis
+		# PAF = DFF.M1 -> DFF = PAF.M1_inv
+
+		if self.isStruct: # Calculate from calcExtAngle.tcl on Chimera structure
+			M1 = self.R3(-43.2).dot(self.R1(90.0))
+			extAlpha = 63.4
+			extBeta = 58.2
+			extGamma = 70.0
+			betaNHRad = 14.0 * pi/180.0
+		else: # Using ideal peptide plane geometry
+			M1 = self.R3(-44).dot(self.R1(90.0))
+			extAlpha = 65.0
+			extBeta = 59.0
+			extGamma = 70.0
+			betaNHRad = 17.0 * pi/180.0
+
+		M1_inv = np.linalg.pinv(M1)
 		extOmega = 180.0
-		betaNHRad = 17.0 * pi/180.0
+
 		R3gamma = self.R3(extGamma)
 		R3alpha = self.R3(extAlpha)
 		R1omega = self.R1(extOmega)
 		R3beta = self.R3(extBeta)
-		# Coorindate of NH vector in the DFF
-		DFF_NH0 = np.array([sin(betaNHRad), 0.0, cos(betaNHRad)])
-		PAF_NH0 = tt.dot(DFF_NH0)
+		# Coorindate of NH vector in the PAF
+		PAF_NH0 = np.array([sin(betaNHRad), 0.0, cos(betaNHRad)])
+		DFF_NH0 = PAF_NH0.dot(M1_inv)
 
 		R = np.eye(3)
 		for k in range(i, j):
@@ -56,12 +67,13 @@ class CalcInterVecAng:
 			psi = self.phipsi_lst[k][1]
 			R1phi = self.R1(phi)
 			R1psi = self.R1(psi)
+			# page 54 of Jun Hu's thesis
 			R = R.dot(R1phi).dot(R3gamma).dot(R1psi).dot(R3alpha).dot(R1omega).dot(R3beta)
 
-		PAF_NH = R.dot(PAF_NH0)
-		dotprod = PAF_NH0.dot(PAF_NH)
+		DFF_NH = DFF_NH0.dot(R)
+		dotprod = DFF_NH0.dot(DFF_NH)
 		interVecAng = acos(dotprod) * 180.0/pi
-		return [resid_i, resid_j, interVecAng]
+		return (resid_i, resid_j, interVecAng)
 
 	def calcAllInterVecAng(self):
 		outfile = open(self.outfilename, 'w')
@@ -69,12 +81,13 @@ class CalcInterVecAng:
 		counter = 0
 		for i in range(0, N-1):
 			for j in range(i+1, N):
-				[resid_i, resid_j, interVecAng] = self.calcOneInterVecAng(i, j)
-				print>>outfile, "%4i%4i%4i%8.3f" % (counter, resid_i, resid_j, interVecAng)
+				(resid_i, resid_j, interVecAng) = self.calcOneInterVecAng(i, j)
+				print>>outfile, '%4i%4i%4i%8.3f' % (counter, resid_i, resid_j, interVecAng)
 				counter += 1
 		outfile.close()
 
 if __name__ == '__main__':
-	#instance = CalcInterVecAng('ideal_phipsi.dat', 'ideal_intervec_ang_pisema.xvg')
-	instance = CalcInterVecAng('phipsi.dat', 'intervec_ang.xvg')
+	instance = CalcInterVecAng('ideal_phipsi.dat', 'ideal_intervec_ang_pisema.xvg', isStruct=True)
+	instance.calcAllInterVecAng()
+	instance = CalcInterVecAng('phipsi.dat', 'intervec_ang.xvg', isStruct=False)
 	instance.calcAllInterVecAng()
